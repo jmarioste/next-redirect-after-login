0c1e0a3e2be5a4bcc6d9b14aa9a4f8d0d58ac60b
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-12-14 19:52:54 +0800

    Remove uncessesary files

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,26 @@
-import { stackMiddlewares } from "middlewares/stackMiddlewares";
-import { withAuthorization } from "middlewares/withAuthorization";
-export default stackMiddlewares([withAuthorization]);
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+export async function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+  const protectedPaths = ["/"];
+  const isPathProtected = protectedPaths?.some((path) => pathname == path);
+  const res = NextResponse.next();
+  if (isPathProtected) {
+    const token = await getToken({ req });
+    if (!token) {
+      const url = new URL(`/api/auth/signin`, req.url);
+      url.searchParams.set("callbackUrl", encodeURI(req.url));
+      return NextResponse.redirect(url);
+    }
+  }
+
+  if (pathname == `/api/auth/signin`) {
+    const token = await getToken({ req });
+    if (token) {
+      const url = new URL("/", req.url);
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return res;
+}
